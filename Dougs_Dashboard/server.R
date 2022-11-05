a17e4082681f92ec57f749bab692621e44db3173
Douglas Wardell-Johnson <Douglaswj@gmail.com>
2022-11-05 15:33:30 +0000

    Update server.R
    Revert back to simple2

--- Dougs_Dashboard/server.R
+++ Dougs_Dashboard/server.R
@@ -114,7 +114,7 @@ function(input, output, session) {
                               daterange <- get_daterange()
                               #map_dta <- filter(travel_lines,traveltype %in% filtervs & start_time_utc >= daterange[1] & end_time_utc <= daterange[2])
                               
-                              query <- str_c("select track_id,colourv,geom,traveltype,start_time_utc,start_time_local,time_taken,length2d_km,kg_all_co2e from dougtracks.dougtracks_lines_emi_mv_simple3
+                              query <- str_c("select track_id,colourv,geom,traveltype,start_time_utc,start_time_local,time_taken,length2d_km,kg_all_co2e from dougtracks.dougtracks_lines_emi_mv_simple2
                    where start_time_utc >= to_date('",daterange[1],"','YYYY-MM-DD') and end_time_utc <= to_date('",daterange[2],"','YYYY-MM-DD')
 and traveltype in (",str_c("'",filtervs,"'",collapse = ","),")")
                               
@@ -813,7 +813,7 @@ group by traveltype_superclass,superclass_colourv,year")
       #daterange <- get_daterange()
       #map_dta <- filter(travel_lines,traveltype %in% filtervs & start_time_utc >= daterange[1] & end_time_utc <= daterange[2])
       
-      query <- str_c("select colourv,geom,traveltype,start_time_utc,start_time_local,time_taken,length2d_km,kg_all_co2e from dougtracks.dougtracks_lines_emi_mv_simple3")
+      query <- str_c("select colourv,geom,traveltype,start_time_utc,start_time_local,time_taken,length2d_km,kg_all_co2e from dougtracks.dougtracks_lines_emi_mv_simple2")
                      #where traveltype = 'Walk'")
       
       map_dta <- st_read(dsn=pgconn,query=query)
