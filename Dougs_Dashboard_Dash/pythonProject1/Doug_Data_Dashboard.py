0967b933be0712f30be1c0de803ed5df887ee746
Douglas Wardell-Johnson <Douglaswj@gmail.com>
2024-08-07 17:25:28 +0100

    Begin Flesh out Dash app
    Begin Fleshing out the Dash app with a bit more content, mostly from tutorials

--- Dougs_Dashboard_Dash/pythonProject1/Doug_Data_Dashboard.py
+++ Dougs_Dashboard_Dash/pythonProject1/Doug_Data_Dashboard.py
@@ -1,6 +1,37 @@
-from dash import Dash, html, dash_table, dcc
+from dash import Dash, html, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 import psycopg
+import os
+from dotenv import load_dotenv
 
-with psycopg.connect("dbname=")
+load_dotenv()
+
+conn = psycopg.connect(host='192.168.1.126',
+                       port=5433,
+                       user='doug',
+                       password=os.environ.get('PG_PASSWORD'),
+                       dbname='spatial_db')
+
+sql = "select traveltype,emi_year as year,sum(length2d_km) as length from dougtracks.dougtracks_lines_mv GROUP BY traveltype,emi_year ORDER BY emi_year ASC;"
+
+df = pd.read_sql_query(sql, conn)
+
+app = Dash()
+
+app.layout = [
+    html.H1(children='App Title',style={'textAlign':'center'}),
+    dcc.Dropdown(df.traveltype.unique(),'Walks',id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+]
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.traveltype==value]
+    return px.line(dff, x='year', y='length')
+
+if __name__ == '__main__':
+    app.run(debug=True)
